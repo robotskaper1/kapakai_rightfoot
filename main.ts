c6e64daf8e56b79c444edf76dc2e33f8addaf37b
KAPAK-AI <115808762+robotskaper1@users.noreply.github.com>
2022-10-23 09:00:04 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,3 @@
-function LeftHip () {
-    while (loop) {
-        if (input.buttonIsPressed(Button.AB)) {
-            basic.showNumber(chosenangle)
-            runservo = 1
-            basic.showIcon(IconNames.Yes)
-        } else if (input.buttonIsPressed(Button.A)) {
-            adjNum += 10
-            chosenangle = adjNum
-            basic.showNumber(chosenangle)
-        } else if (input.buttonIsPressed(Button.B)) {
-            adjNum += -10
-            chosenangle = adjNum
-            basic.showNumber(chosenangle)
-        } else if (adjNum > 180) {
-            basic.showIcon(IconNames.Sad)
-            basic.showNumber(adjNum)
-        } else if (runservo != 0) {
-            basic.showNumber(chosenangle)
-            kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, chosenangle)
-            basic.pause(200)
-            kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, 0)
-        }
-    }
-    runservo = 0
-    LeftHipSetting = chosenangle
-}
 function RightFoot () {
     while (loop) {
         if (input.buttonIsPressed(Button.AB)) {
@@ -53,7 +26,6 @@ function RightFoot () {
     RigthFootSetting = chosenangle
 }
 let RigthFootSetting = 0
-let LeftHipSetting = 0
 let chosenangle = 0
 let adjNum = 0
 let runservo = 0
@@ -68,5 +40,4 @@ adjNum = 0
 let configmore = 0
 basic.forever(function () {
     RightFoot()
-    LeftHip()
 })
